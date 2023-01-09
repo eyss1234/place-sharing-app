4a984075c2192c9362e35faf7f195ee355376769
Emily Santos <eyssantos@gmail.com>
2023-01-09 12:12:51 +0000

    use card and avatar components into user item

--- frontend/src/users/pages/Users.js
+++ frontend/src/users/pages/Users.js
@@ -2,7 +2,7 @@ import React from "react";
 import UsersList from "../components/UsersList";
 
 const Users = () => {
-  const USERS = [{id:'u1', name:'Em', image:'https://source.unsplash.com/hwLAI5lRhdM', places:3}];
+  const USERS = [{id:'u1', name:'Em', image:'https://source.unsplash.com/pOUA8Xay514', places:3}];
 
   return <UsersList items={USERS} />
 };

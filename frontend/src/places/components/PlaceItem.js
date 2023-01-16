3c20b39e79b85b5884c9fb7f39f4a01140716de6
Emily Santos <eyssantos@gmail.com>
2023-01-16 17:39:26 +0000

    display edit and delete button on placeItem only when logged in

--- frontend/src/places/components/PlaceItem.js
+++ frontend/src/places/components/PlaceItem.js
@@ -1,12 +1,15 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 
 import Card from "../../shared/components/UIElements/Card";
 import Button from "../../shared/components/FormElements/Button";
 import Modal from "../../shared/components/UIElements/Modal";
 import Map from "../../shared/components/UIElements/Map";
+import { AuthContext } from "../../shared/context/auth-context";
 import './PlaceItem.css'
 
 const PlaceItem = props => {
+  const auth = useContext(AuthContext);
+
   const [showMap, setShowMap] = useState(false);
   const [showConfirmModal, setShowConfirmModal] = useState(false);
 
@@ -55,6 +58,7 @@ const PlaceItem = props => {
     }>
       <p>Do you want to proceed and delete this place? Please not that this action cannot be undone thereafter.</p>
     </Modal>
+    
     <li className="place-item">
     <Card className="place-item__content">
       <div className="place-item__image">
@@ -67,8 +71,8 @@ const PlaceItem = props => {
       </div>
       <div className="place-item__actions">
         <Button inverse onClick={openMapHandler}>VIEW ON MAP</Button>
-        <Button to={`/places/${props.id}`}>EDIT</Button>
-        <Button danger onClick={showDeleteWarningHandler}>DELETE</Button>
+        {auth.isLoggedIn && <Button to={`/places/${props.id}`}>EDIT</Button>}
+        {auth.isLoggedIn && <Button danger onClick={showDeleteWarningHandler}>DELETE</Button>}
       </div>
       </Card>
     </li>

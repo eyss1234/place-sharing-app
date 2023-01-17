7d76352300b2051460f049aebb1c1ec6a64d4309
Emily Santos <eyssantos@gmail.com>
2023-01-17 16:01:03 +0000

    create route for places/:pid

--- backend/routes/places-routes.js
+++ backend/routes/places-routes.js
@@ -2,9 +2,26 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  console.log('GET Request in Places');
-  res.json({message: 'It works!'});
+const DUMMY_PLACES = [
+  {
+    id: 'p1',
+    title: 'Shinjuku Gojira',
+    description: 'Charismatic monster in a neighbourhood in Japan',
+    location: {
+      lat: 35.6952948,
+      lng: 139.7008743
+    },
+    address: '1-19-1, Kabukicho, Shinjuku-ku, Tokyo, 160-0021',
+    creater: 'u1'
+  }
+];
+
+router.get('/:pid', (req, res, next) => {
+  const placeId = req.params.pid;
+  const place = DUMMY_PLACES.find(p => {
+    return p.id === placeId;
+  });
+  res.json({place});
 });
 
 module.exports = router;

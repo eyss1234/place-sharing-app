9facf549129000263a13a224d46738411ad4765b
Emily Santos <eyssantos@gmail.com>
2023-01-18 11:27:40 +0000

    create post request and test with postman

--- backend/controllers/places-controller.js
+++ backend/controllers/places-controller.js
@@ -27,17 +27,32 @@ const getPlaceById =  (req, res, next) => {
 }
 
 const getPlacebyUserId = (req, res, next) => {
-    const userId = req.params.uid;
-    const place = DUMMY_PLACES.find(p => {
-      return p.creator === userId;
-    })
-    if (!place) {
-      return next(new HttpError(
-        'Could not find a user for the provided user id.', 404
-      ));
-    }
-    res.json({place});
+  const userId = req.params.uid;
+  const place = DUMMY_PLACES.find(p => {
+  return p.creator === userId;
+  })
+  if (!place) {
+    return next(new HttpError(
+      'Could not find a user for the provided user id.', 404
+    ));
   }
+  res.json({place});
+}
+
+const createPlace = (req, res, next) => {
+  const { title, description, coordinates, address, creator } = req.body;
+  const createdPlace = {
+    title,
+    description,
+    location: coordinates,
+    address,
+    creator
+  };
+  DUMMY_PLACES.push(createdPlace);
+
+  res.status(201).json({place: createdPlace})
+}
 
 exports.getPlaceById = getPlaceById;
 exports.getPlacebyUserId  = getPlacebyUserId ;
+exports.createPlace = createPlace;
